a826cb4942fbf4c54c7e3d7cc80e12c80446116f
filhot3 <jhoellyscarvalho@gmail.com>
2023-11-14 03:34:25 -0300

    URL Server Update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("ws://chat-online-box.onrender.com");
     websocket.onmessage = processMessage;
 };
 
