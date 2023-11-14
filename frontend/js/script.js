7fe78d0c25b1b3f446fd39bdaac46d8b3addd386
Jhoellys Carvalho <76889229+filhot3@users.noreply.github.com>
2023-11-14 04:00:06 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("https://chat-box-h7r6.onrender.com/")
+    websocket = new WebSocket("ws://chat-box-h7r6.onrender.com/")
     websocket.onmessage = processMessage
 }
 
